8967877815a7ffd2d5acc6df760b94415097f3a4
adrock_sim <sjhwo9370@adrock.me>
2024-11-08 14:27:52 +0900

    feat: 🎸 prettierrc | eslintrc | env | Babel 추가

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+  ],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: ["react", "react-native"],
+  rules: {
+    "react/prop-types": "off",
+    "no-unused-vars": "warn",
+    "react-native/no-inline-styles": "off",
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          allowUndefined: false,
+        },
+      ],
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+    ],
   };
 };
