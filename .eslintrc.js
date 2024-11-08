b54316baf9f8428a722f3706b2f9c9546884a222
adrock_sim <sjhwo9370@adrock.me>
2024-11-08 15:04:11 +0900

    fix: 🐛 Prettier 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
-  ],
-  parser: "@babel/eslint-parser",
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    requireConfigFile: false,
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  plugins: ["react", "react-native"],
-  rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
-    "react-native/no-inline-styles": "off",
-  },
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:react-native/all'
+    ],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        requireConfigFile: false,
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    plugins: ['react', 'react-native'],
+    rules: {
+        'react/prop-types': 'off',
+        'no-unused-vars': 'warn',
+        'react-native/no-inline-styles': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {}, // TypeScript 경로 매핑 지원
+        },
+    },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+const config = {
+  trailingComma: 'all', // 모든 요소 뒤에 쉼표 추가
+  tabWidth: 4, // 들여쓰기 4칸
+  useTabs: false, // 탭 대신 공백 사용
+  semi: true, // 세미콜론 추가
+  singleQuote: true, // 문자열에 싱글 쿼트 사용
+  printWidth: 80, // 한 줄 최대 길이 80자
+  bracketSpacing: true, // 중괄호 내부에 공백 추가
+  arrowParens: 'always', // 화살표 함수 매개변수에 괄호 항상 추가
+  quoteProps: 'as-needed', // 필요한 경우에만 객체 속성에 따옴표 추가
+};
+
+module.exports = config;
